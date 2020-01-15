cc83d4b0d14502f6c41fc1a3ecb9db3c04a08e23
Vineet Kumar Arya <vineet.kumar.arya@gmail.com>
2020-01-15 17:31:48 +0530

    updated files locally from remote

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,20 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DateTimeComponent } from './date-time/date-time.component';
 import { AddressCardComponent } from './address-card/address-card.component';
+import { HeaderComponentComponent } from './header-component/header-component.component';
+import { FooterComponentComponent } from './footer-component/footer-component.component';
+import { MainComponentComponent } from './main-component/main-component.component';
+import { SidebarComponentComponent } from './sidebar-component/sidebar-component.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     DateTimeComponent,
-    AddressCardComponent
+    AddressCardComponent,
+    HeaderComponentComponent,
+    FooterComponentComponent,
+    MainComponentComponent,
+    SidebarComponentComponent
   ],
   imports: [
     BrowserModule,
